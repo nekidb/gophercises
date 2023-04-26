40a9eb551ac207be853ad1663838dc59256ac938
nekidb <nekidb@gmail.com>
2023-04-26 22:59:56 +0300

    added cli structure

---
+++ task/cmd/list.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all of your incomplete tasks",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("You have not any tasks")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
